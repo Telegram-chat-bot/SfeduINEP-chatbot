040e583fbd854536c0c8bec472fb291bf8a6ba5a
Pr0gger1 <kraus2202200491@gmail.com>
2021-10-10 11:12:36 +0300

    обновлена архитектура бота

---
+++ loader.py
@@ -0,0 +1,14 @@
+from aiogram import Dispatcher, Bot, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from data import config
+import logging
+
+bot = Bot(token=config.BOT_TOKEN)
+storage = MemoryStorage()
+
+dp = Dispatcher(bot, storage=storage)
+
+
+#Логгирование
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s',level=logging.INFO,)
