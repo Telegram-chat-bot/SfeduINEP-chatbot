840e0b60b679e81cdb3dab06e4637ed57b4bdca7
Pr0gger1 <kraus2202200491@gmail.com>
2021-12-27 00:32:20 +0300

    фикс отправки id юзера в бд

--- handlers/users/commands/user_commands.py
+++ handlers/users/commands/user_commands.py
@@ -16,6 +16,7 @@ async def welcome(message: Message):
         await db_commands.get_welcome_msg(),
         reply_markup=kb.main_menu
     )
+
     await db_commands.add_user(
         name=' '.join(
             [
@@ -24,7 +25,7 @@ async def welcome(message: Message):
             ]
         ),
         uid=int(message.from_user.id)
-    )
+    ) or None
 
 
 @dp.message_handler(IsChat(), Command("help"))
